41fd94776d5e117c4b5be2ee258076bcbf525b84
Bao Phung <bao.phungus.26@gmail.com>
2023-11-30 10:04:47 -0800

    Delete League/LeagueProfileFetcher directory

--- League/LeagueProfileFetcher/CustomException.cpp
+++
@@ -1,9 +0,0 @@
-#include "./CustomException.h"
-
-CustomException::CustomException(QString error_message){
-    this->error_message = error_message;
-}
-
-void CustomException::print_message(){
-    cout << "Error description: " << this->error_message.toStdString() << endl;
-}
